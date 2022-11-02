03606d47a3a3c672dd2747ade1f4958b7c70d6f3
LaurenC26 <d00262382@student.dkit.ie>
2022-11-02 19:55:48 +0000

    eleventh commit

--- raylibStartTemplates-master/CA1_RaylibGame.cpp
+++ raylibStartTemplates-master/CA1_RaylibGame.cpp
@@ -13,7 +13,7 @@ struct Ball
 	void Draw()
 	{
 //this code is drawing the ball and since we have defined the ball we just have to call the structs. 
-			DrawCircle((int) x, y,(int) radius, GOLD);
+			DrawCircle((int) x, y,(int) radius, RAYWHITE);
 	}
 };
 //the struct code for the paddle.
@@ -31,7 +31,7 @@ struct Paddle
 	void Draw()
 	{
 		//DrawRectangleGradientEx (GetRect(), LIME,LIME,RED,RED);
-	DrawRectangleRec(GetRect(), GOLD); 
+	DrawRectangleRec(GetRect(), RAYWHITE); 
 	//THE draw rectangle grad ex only needs the rec position and color.
 	};
 };
@@ -43,25 +43,27 @@ int main(){
 	InitWindow(800, 450, "CyberPong");
 	//set Window state makes sure that the refresh of the FPS stays inline with the computer refresh window rate.
 	SetWindowState(FLAG_VSYNC_HINT);
-
+	//initilizes audio
 	InitAudioDevice();
-	
+	//defining music
 	Music music =
 	LoadMusicStream("Resources/Music.wav");
 	PlayMusicStream (music);
-
+	//defining seperatie types of music
 	Sound soundPong =LoadSound ("Resources/sound.rfx.wav");
 	Sound winSound =LoadSound ("Resources/win.wav");
-
+	//searching resources
 	Texture2D background = LoadTexture("resources/cyberpunk_street_background.png");
     Texture2D midground = LoadTexture("resources/cyberpunk_street_midground.png");
     Texture2D foreground = LoadTexture("resources/cyberpunk_street_foreground.png");
-
+	//adding floats
     float scrollingBack = 0.0f;
     float scrollingMid = 0.0f;
     float scrollingFore = 0.0f;
 	bool scored = false;
+	bool started = false;
 // float type because they dont always have to be integers.
+
 //.0f divides by a float
 //struct to define the ball.
 	Ball ball;
@@ -121,147 +123,157 @@ int main(){
 		//BeginDrawing();
 		//draws a clear background
 		//ClearBackground(BLACK);
-//draws the fps rate if you hover it takes two arguments position x and y. X horizontal, y vertically. (0,0) 
-//corresponds to the left of the box. origin is the top left and y goes down as the number gets bigger.
+		//draws the fps rate if you hover it takes two arguments position x and y. X horizontal, y vertically. (0,0) 
+		//corresponds to the left of the box. origin is the top left and y goes down as the number gets bigger.
 
-//could put the figues of the circle, but you can get the width of the screen and divide by 2 and same with height.
-			//DrawCircle(GetScreenWidth()/2, GetScreenHeight()/2, 5, WHITE);
-//For the rectangle the height is not centred, the circle draws from the centre where rectangle draws from the top left, to fix this you substract half of the height to put it as the center.
-			//DrawRectangle(50, GetScreenHeight()/2 - 50, 10, 100, WHITE);
-//for width subtract 50 cuz we want to draw it 50 from the right but it draws from the left and not the center we also need to substract the width, it will make it so that the right of the right is 50 away from the width.
-			//DrawRectangle (GetScreenWidth()-50 -10, GetScreenHeight()/2-50, 10, 100, WHITE);
-			//DrawFPS(10,10);
-//ends the drawing
-		//EndDrawing();
-//only drawn in one position, to move we have to replace the constants with variables, named floats above and changed the below of the drawing. 
+		//could put the figues of the circle, but you can get the width of the screen and divide by 2 and same with height.
+					//DrawCircle(GetScreenWidth()/2, GetScreenHeight()/2, 5, WHITE);
+		//For the rectangle the height is not centred, the circle draws from the centre where rectangle draws from the top left, to fix this you substract half of the height to put it as the center.
+					//DrawRectangle(50, GetScreenHeight()/2 - 50, 10, 100, WHITE);
+		//for width subtract 50 cuz we want to draw it 50 from the right but it draws from the left and not the center we also need to substract the width, it will make it so that the right of the right is 50 away from the width.
+					//DrawRectangle (GetScreenWidth()-50 -10, GetScreenHeight()/2-50, 10, 100, WHITE);
+					//DrawFPS(10,10);
+		//ends the drawing
+				//EndDrawing();
+		//only drawn in one position, to move we have to replace the constants with variables, named floats above and changed the below of the drawing. 
+
+		//if statement for main menu
+		if(started == false){
+			if(IsKeyDown(KEY_SPACE))
+			{
+				started = true;
+			}
+		}
+		//if if statement true it plays the game
+		if(started==true){
+			//put movement before drawing.
+			//ball slow need because of frame rate if multiplied by 1 it will be very slow, you multiply by a bigger number. Inputted Variables from the top and called by BallSpeedX
+
+			ball.x += ball.SpeedX * GetFrameTime();
+			ball.y += ball.SpeedY * GetFrameTime();
+
+			if (ball.y < 0)
+			{
+				ball.y= 0;
+				ball.SpeedY *= -1;
+			}
+
+			//add logic to the ball to stop it moving off the screen.
+
+			if (ball.y > GetScreenHeight())
+			{ 
+				//stops ball getting stuck in the bottom of the screen.
+				ball.y =GetScreenHeight();
+				// -1 reverses the ball.
+				ball.SpeedY *= -1;
+			}
+
+			//this code means that if the position of the right paddle on the y axis is less than 50 then it will stay the same.
+			if (rightPaddle.y < rightPaddle.height/2)
+			{
+				rightPaddle.y=rightPaddle.height/2;
+			}
+
+			//this is reverseing the above but it is getting the screen height of 800 and minusing 50.
+			if (rightPaddle.y > GetScreenHeight() - rightPaddle.height/2)
+			{
+				rightPaddle.y= GetScreenHeight() - rightPaddle.height/2;
+			}
+
+			//this code means that if the position of the left paddle on the y axis is less than 50 then it will stay the same.
+			if (leftPaddle.y < leftPaddle.height/2)
+			{
+				leftPaddle.y=leftPaddle.height/2;
+			}
+
+			//this is reverseing the above but it is getting the screen height of 800 and minusing 50.
+			if (leftPaddle.y > GetScreenHeight() - leftPaddle.height/2)
+			{
+				leftPaddle.y= GetScreenHeight() - leftPaddle.height/2;
+			}
+			//if w is down it will move left paddle up
+			if(IsKeyDown(KEY_W))
+			{
+				leftPaddle.y -= leftPaddle.speed * GetFrameTime();
+			}
+			//if s is down it will move paddle down
+			if(IsKeyDown(KEY_S))
+			{
+				leftPaddle.y += leftPaddle.speed * GetFrameTime();
+			}
+			//if the ball is greater than the half way point it will move the right paddle
+			if (ball.x > GetScreenWidth()/ 2 )
+			{
+				if(ball.y < rightPaddle.y)
+				{
+					rightPaddle.y -= rightPaddle.speed/2 * GetFrameTime();
+				}
+
+				if(ball.y > rightPaddle.y)
+				{
+					rightPaddle.y += rightPaddle.speed/2 * GetFrameTime();
+				}
+			}
+				
+			if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius,leftPaddle.GetRect()))
+			{
+				if(ball.SpeedX < 0)
+				{
+				ball.SpeedX *= -1.1f;
+				ball.SpeedY = (ball.y -leftPaddle.y) / (leftPaddle.height/2) * ball.SpeedX;
+				PlaySound(soundPong);
+				}
+			}
+
+				
+			if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius,rightPaddle.GetRect()))
+			{
+				if(ball.SpeedX> 0)
+				{
+				ball.SpeedX *= -1.1f;
+				ball.SpeedY = (ball.y -rightPaddle.y) / (rightPaddle.height/2) * -ball.SpeedX;
+				PlaySound(soundPong);
+				}
+			}
 
 
-//put movement before drawing.
-//ball slow need because of frame rate if multiplied by 1 it will be very slow, you multiply by a bigger number. Inputted Variables from the top and called by BallSpeedX
-
-ball.x += ball.SpeedX * GetFrameTime();
-ball.y += ball.SpeedY * GetFrameTime();
-
-if (ball.y < 0)
-{
-	ball.y= 0;
-	ball.SpeedY *= -1;
-}
-
-//add logic to the ball to stop it moving off the screen.
-
-if (ball.y > GetScreenHeight())
-{ 
-	//stops ball getting stuck in the bottom of the screen.
-	ball.y =GetScreenHeight();
-	// -1 reverses the ball.
-	ball.SpeedY *= -1;
-}
-
-//this code means that if the position of the right paddle on the y axis is less than 50 then it will stay the same.
-if (rightPaddle.y < rightPaddle.height/2)
-{
-	rightPaddle.y=rightPaddle.height/2;
-}
-
-//this is reverseing the above but it is getting the screen height of 800 and minusing 50.
-if (rightPaddle.y > GetScreenHeight() - rightPaddle.height/2)
-{
-	rightPaddle.y= GetScreenHeight() - rightPaddle.height/2;
-}
-
-//this code means that if the position of the left paddle on the y axis is less than 50 then it will stay the same.
-if (leftPaddle.y < leftPaddle.height/2)
-{
-	leftPaddle.y=leftPaddle.height/2;
-}
-
-//this is reverseing the above but it is getting the screen height of 800 and minusing 50.
-if (leftPaddle.y > GetScreenHeight() - leftPaddle.height/2)
-{
-	leftPaddle.y= GetScreenHeight() - leftPaddle.height/2;
-}
-
-if(IsKeyDown(KEY_W))
-{
-	leftPaddle.y -= leftPaddle.speed * GetFrameTime();
-}
-
-if(IsKeyDown(KEY_S))
-{
-	leftPaddle.y += leftPaddle.speed * GetFrameTime();
-}
-
-if(IsKeyDown(KEY_UP))
-{
-	rightPaddle.y -= rightPaddle.speed * GetFrameTime();
-}
-
-if(IsKeyDown(KEY_DOWN))
-{
-	rightPaddle.y += rightPaddle.speed * GetFrameTime();
-}
-
-	
-if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius,leftPaddle.GetRect()))
-{
-	if(ball.SpeedX < 0)
-	{
-	ball.SpeedX *= -1.1f;
-	ball.SpeedY = (ball.y -leftPaddle.y) / (leftPaddle.height/2) * ball.SpeedX;
-	PlaySound(soundPong);
-	}
-}
-
-	
-if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius,rightPaddle.GetRect()))
-{
-	if(ball.SpeedX> 0)
-	{
-	ball.SpeedX *= -1.1f;
-	ball.SpeedY = (ball.y -rightPaddle.y) / (rightPaddle.height/2) * -ball.SpeedX;
-	PlaySound(soundPong);
-	}
-}
+			if(ball.x<0)
+			{
+				winnerText = "Right Player Wins!";
+				
+				if (scored ==false){
+					PlaySound(winSound);
+				}
+				scored = true;
+			}
 
 
-if(ball.x<0)
-{
-	winnerText = "Right Player Wins!";
-	
-	if (scored ==false){
-		PlaySound(winSound);
-	}
-	scored = true;
-}
+			if(ball.x > GetScreenWidth())
+			{
+				winnerText = "Left Player Wins!";
+				if (scored ==false){
+					PlaySound(winSound);
+				}
+				scored = true;
+			}
 
-
-if(ball.x > GetScreenWidth())
-{
-	winnerText = "Left Player Wins!";
-	if (scored ==false){
-		PlaySound(winSound);
-	}
-	scored = true;
-}
-
-if (winnerText && IsKeyPressed(KEY_SPACE))
-{
-	ball.x = GetScreenWidth() / 2;
-	ball.y =GetScreenHeight() / 2;
-	ball.SpeedX = 300;
-	ball.SpeedY = 300;
-	winnerText = nullptr;
-	scored = false;
-	StopSound(winSound);
-}
-
-BeginDrawing();
-		//draws a clear background//begins the rendering and creates a draw section.	
+			if (winnerText && IsKeyPressed(KEY_SPACE))
+			{
+				ball.x = GetScreenWidth() / 2;
+				ball.y =GetScreenHeight() / 2;
+				ball.SpeedX = 300;
+				ball.SpeedY = 300;
+				winnerText = nullptr;
+				scored = false;
+				StopSound(winSound);
+			}
+		}
+		BeginDrawing();
+			//draws a clear background//begins the rendering and creates a draw section.	
 			ClearBackground(GetColor(0x052c46ff));
-//draws the fps rate if you hover it takes two arguments position x and y. X horizontal, y vertically. (0,0) 
-//corresponds to the left of the box. origin is the top left and y goes down as the number gets bigger.
-   // NOTE: Texture is scaled twice its size
+			//draws the fps rate if you hover it takes two arguments position x and y. X horizontal, y vertically. (0,0) 
+			//corresponds to the left of the box. origin is the top left and y goes down as the number gets bigger.
+   			// NOTE: Texture is scaled twice its size
             DrawTextureEx(background, (Vector2){ scrollingBack, 20 }, 0.0f, 2.0f, WHITE);
             DrawTextureEx(background, (Vector2){ background.width*2 + scrollingBack, 20 }, 0.0f, 2.0f, WHITE);
 
@@ -274,24 +286,35 @@ BeginDrawing();
             DrawTextureEx(foreground, (Vector2){ foreground.width*2 + scrollingFore, 70 }, 0.0f, 2.0f, WHITE);
 
             		
-			
-			ball.Draw();
-			//it will continue to draw the ball as it did.
-//For the rectangle the height is not centred, the circle draws from the centre where rectangle draws from the top left, to fix this you substract half of the height to put it as the center.
-			leftPaddle.Draw();
-			rightPaddle.Draw();
+			if(started == true){
+				ball.Draw();
+				//it will continue to draw the ball as it did.
+				//For the rectangle the height is not centred, the circle draws from the centre where rectangle draws from the top left, to fix this you substract half of the height to put it as the center.
+				leftPaddle.Draw();
+				rightPaddle.Draw();
 
-			if (winnerText)
-			{
-				int textWidth = MeasureText(winnerText, 60);
-				DrawText (winnerText, GetScreenWidth() / 2 -textWidth / 2, GetScreenHeight() / 2 -30, 60, YELLOW);		
+				if (winnerText)
+				{
+					int textWidth = MeasureText(winnerText, 60);
+					DrawText (winnerText, GetScreenWidth() / 2 -textWidth / 2, GetScreenHeight() / 2 -30, 60, RAYWHITE);
+					//press space to start next round		
+					DrawText ("Press the Spacebar to continue", GetScreenWidth() / 2 -textWidth / 2 + 30, GetScreenHeight() / 2 +100, 30, RAYWHITE);
+				}
 			}
+			else{
 
+				DrawText ("CYBERPONG", GetScreenWidth() / 3-105, GetScreenHeight() / 2-50, 80, RAYWHITE);
+				DrawText ("Press W to move paddle up", GetScreenWidth() / 2-200, GetScreenHeight() / 2 +50, 30, RAYWHITE);
+				DrawText ("Press S to move paddle up", GetScreenWidth() / 2-200, GetScreenHeight() / 2 +90, 30, RAYWHITE);
+				DrawText ("Press the Spacebar to play", GetScreenWidth() / 2-210, GetScreenHeight() / 2 +130, 30, RAYWHITE);
+
+				//main menu stuff
+			}
 			//for width subtract 50 cuz we want to draw it 50 from the right but it draws from the left and not the center we also need to substract the width, it will make it so that the right of the right is 50 away from the width.
 			//DrawRectangle (GetScreenWidth()/2 -50 -10, GetScreenHeight()/2-50, 10, 100, WHITE);
-//only drawn in one position, to move we have to replace the constants with variables. 
+			//only drawn in one position, to move we have to replace the constants with variables. 
 			DrawFPS(10,10);
-//ends the drawing
+			//ends the drawing
 		EndDrawing();
 	
 	}
